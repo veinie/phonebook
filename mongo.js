c4ec3c9626e38e92d8b2effdb9de845b61b0641c
veiniem <veikka.niemitukia@student.hamk.fi>
2023-08-30 19:57:07 +0300

    Added mongoose and mongo.js

---
+++ mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as an argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://veinie:${password}@cluster0.lb9e8sw.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length == 5) {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+    })
+
+    person.save().then(result => {
+        console.log(`added ${person.name} ${person.number} to phonebook`)
+        mongoose.connection.close()
+        process.exit(0)
+    })
+}
+
+if (process.argv.length == 3) {
+    console.log('phonebook:')
+    Person.find({}).then(persons => {
+        persons.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+        process.exit(1)
+    })
+}
