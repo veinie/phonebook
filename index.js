c9415f2c2ab4edb729db7093fb05324db1ebedd7
veiniem <veikka.niemitukia@student.hamk.fi>
2023-09-02 18:51:40 +0300

    uibuild

--- index.js
+++ index.js
@@ -21,9 +21,15 @@ const errorHandler = (error, request, response, next) =>{
 
     if (error.name === 'CastError') {
         return response.status(400).send({error: 'malformatted id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
     }
 
     next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint'})
 }
 
 app.use(express.json())
@@ -66,31 +72,25 @@ app.delete('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
-    const body = request.body
-    if (!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'name and number must be included'
+app.post('/api/persons', (request, response, next) => {
+    const {name, number} = request.body
+    const person = new Person({name, number})
+    person.save()
+        .then(result => {
+            console.log(`added ${person.name} ${person.number} to phonebook`)
+            response.json(person)        
         })
-    }
-    const person = new Person({
-        name: body.name,
-        number: body.number
-    })
-    person.save().then(result => {
-        console.log(`added ${person.name} ${person.number} to phonebook`)        
-    })
-    response.json(person)
+        .catch(error => next(error))
     morgan.token('body', request => JSON.stringify(request.body))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
-    const person = {
-        name: body.name,
-        number: body.number
-    }
-    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    const {name, number} = request.body
+    Person.findByIdAndUpdate(
+        request.params.id,
+        {name, number},
+        {new: true, runValidators: true, context: 'query'}
+    )
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
@@ -98,6 +98,7 @@ app.put('/api/persons/:id', (request, response, next) => {
 })
 
 app.use(errorHandler)
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
